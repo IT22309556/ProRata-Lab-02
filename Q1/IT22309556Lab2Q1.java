f029d5d6d06441142c7264042d8e4c6e955f5393
IT22309556 <it22309556@my.sliit.lk>
2026-09-24 13:13:35 +0530

    Add IT22309556Lab2Q1.java to Q1

---
+++ Q1/IT22309556Lab2Q1.java
@@ -0,0 +1,13 @@
+public class IT22309556Lab2Q1 {
+    public static void main(String[] args) {
+
+        double perimeter = 100;
+        double length, width;
+
+        length = 100 / 3.5;   // from equation
+        width = length * 0.75;
+
+        System.out.println(length);
+        System.out.println(width);
+    }
+}
